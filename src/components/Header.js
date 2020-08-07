481d07455e3434242f894df82df75b42ee221b7e
gijoe2day <gjuliao32@hotmail.com>
2020-08-06 22:32:41 -0500

    finished version

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+    <div>
+      <h2>Expense Tracker</h2>
+    </div>
+  );
+};
---
+++ src/context/GlobalState.js
@@ -0,0 +1,42 @@
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+const initialState = {
+  transactions: [],
+};
+
+//create context
+
+export const GlobalContext = createContext(initialState);
+
+//create provider
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
