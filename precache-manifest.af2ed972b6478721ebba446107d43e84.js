a04eabc128f23c63999620560f70aa60fedc5ffa
gijoe2day <gjuliao32@hotmail.com>
2020-08-13 08:38:02 -0500

    Updates

---
+++ precache-manifest.af2ed972b6478721ebba446107d43e84.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f0333fc9e47365860679564086390c",
+    "url": "/React-Expense-Tracker/index.html"
+  },
+  {
+    "revision": "3543e57a7b92e9755339",
+    "url": "/React-Expense-Tracker/static/css/main.88b92223.chunk.css"
+  },
+  {
+    "revision": "ef9f5bd7b17e4d058f9f",
+    "url": "/React-Expense-Tracker/static/js/2.5a84a2c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Expense-Tracker/static/js/2.5a84a2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3543e57a7b92e9755339",
+    "url": "/React-Expense-Tracker/static/js/main.ad8e66fc.chunk.js"
+  },
+  {
+    "revision": "5a5ad5d400f121bdd00d",
+    "url": "/React-Expense-Tracker/static/js/runtime-main.e6a3f00d.js"
+  }
+]);
